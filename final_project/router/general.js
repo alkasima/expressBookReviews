3f1bb1a93141a9e02a38e8c63c4e92bd50b27300
Kasim Abubakar Jajere <alkasima@gmail.com>
2024-09-10 15:08:07 +0100

    Added async for get book details by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,18 +57,22 @@ public_users.get('/isbn/:isbn', async function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   
-  const authorName = req.params.author.toLowerCase();
+  try {
+        const authorName = req.params.author.toLowerCase();
 
-  const bookList = Object.values(books);
+        const bookList = Object.values(books);
 
-  const booksByAuthor = bookList.filter(book => book.author.toLowerCase() === authorName);
+        const booksByAuthor = bookList.filter(book => book.author.toLowerCase() === authorName);
 
-  if(booksByAuthor.length > 0) {
-    res.json(booksByAuthor);
-  }else {
-    res.status(404).json({message: "Books by this author not found"});
+        if(booksByAuthor.length > 0) {
+          res.json(booksByAuthor);
+        }else {
+          res.status(404).json({message: "Books by this author not found"});
+        }
+  }catch {
+    res.status(500).json({message: 'Error fetching books'});
   }
 });
 
